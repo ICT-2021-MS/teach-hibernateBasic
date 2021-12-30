cc3772da67a7388c52c6ee0c68eb7ba6887f752f
Bissbert <fabian@moor-informatik.ch>
2021-12-30 17:43:54 +0100

    java to easy preset

--- src/ch/bissbert/hibernatebasics/data/Cupboard.java
+++ src/ch/bissbert/hibernatebasics/data/Cupboard.java
@@ -8,18 +8,14 @@ import java.util.List;
 
 /**
  * a simple class having an {@link Integer} id, a list of items({@link Item}) and {@link String} name
- * TODO implement the attributes using hibernate Annotations
  * The id should be automatically incremented
  */
 @Entity
 public class Cupboard {
-    @Id
-    @Column
+
     private int id;
 
-    //todo create annotation
     private List<Item> items;
 
-    @Column
     private String name;
 }
--- src/ch/bissbert/hibernatebasics/data/Item.java
+++ src/ch/bissbert/hibernatebasics/data/Item.java
@@ -6,15 +6,11 @@ import javax.persistence.Id;
 
 /**
  * a simple class that only contains an {@link Integer} id and a {@link String} name
- * TODO implement the attributes using hibernate Annotations
  * the id should be automatically incremented
  */
 @Entity
 public class Item {
-    @Id
-    @Column
     private int id;
 
-    @Column
     private String name;
 }
